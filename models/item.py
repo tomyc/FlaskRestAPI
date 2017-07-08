88a7adbd8a8360183ecba7469c22d6f804612de2
tomas <tomasz@cieplak.net>
2017-07-08 19:51:04 +0200

    Fixed models/item.py

--- models/item.py
+++ models/item.py
@@ -9,7 +9,7 @@ class ItemModel(db.Model):
     name = db.Column(db.String(90))
     price = db.Column(db.Float(precision=2))
 
-    store_id = db.Column(db.INTEGER, db.ForeignKey('stores.id'))
+    store_id = db.Column(db.Integer, db.ForeignKey('stores.id'))
     store = db.relationship('StoreModel')
 
     def __init__(self, name, price, store_id):
